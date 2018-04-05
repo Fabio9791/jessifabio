8810e69e633aca71a6b2b6e106b5bd5b51508588
admin <>
2018-04-05 17:06:42 +0200

    wertzhvuik

--- app/src/main/java/com/neopixl/jessifabiocool/MainActivity.java
+++ app/src/main/java/com/neopixl/jessifabiocool/MainActivity.java
@@ -1,13 +1,32 @@
 package com.neopixl.jessifabiocool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_button)
+    Button bottomButton;
+    @BindView(R.id.imageView)
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bottom_button)
+    public void onViewClicked() {
+
+        imageView.setVisibility(View.VISIBLE);
     }
 }
