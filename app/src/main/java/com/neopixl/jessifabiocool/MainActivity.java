2ff8981d16c9f50bef9485b7b5eb0be4f6888a4f
Jessica Tafani <jessicatafani@MacBook-Pro-de-Jessica.local>
2018-04-05 17:06:22 +0200

    dfghjk

--- app/src/main/java/com/neopixl/jessifabiocool/MainActivity.java
+++ app/src/main/java/com/neopixl/jessifabiocool/MainActivity.java
@@ -1,13 +1,34 @@
 package com.neopixl.jessifabiocool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_jessi)
+    Button buttonJessi;
+    @BindView(R.id.image_coin)
+    ImageView imageCoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_jessi)
+    public void onViewClicked() {
+
+        imageCoin.setVisibility(View.VISIBLE);
+
+
     }
 }
